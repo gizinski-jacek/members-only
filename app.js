691227d664c091de3b7f7cc0d6a46dd0fae26ed2
mrjackmaster <jacektrg+github@gmail.com>
2022-01-12 17:52:58 +0100

    Configuring express-session and passportjs

--- app.js
+++ app.js
@@ -1,39 +1,101 @@
 require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
-var compression = require('compression');
-var sass = require('sass');
-var result = sass.compile('public/stylesheets/style.scss');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcryptjs = require('bcryptjs');
+const compression = require('compression');
+const MongoStore = require('connect-mongo');
+const sass = require('sass');
+const result = sass.compile('public/stylesheets/style.scss');
+const Member = require('./models/member');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var mongoose = require('mongoose');
-var mongoDb = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
+
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		Member.findOne({ username: username }, (err, user) => {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username' });
+			}
+			if (
+				bcryptjs.compare(password, user.password, (err, res) => {
+					if (err) {
+						return done(err);
+					}
+					if (res) {
+						// passwords match! log user in
+						return done(null, user);
+					} else {
+						// passwords do not match!
+						return done(null, false, {
+							message: 'Incorrect password',
+						});
+					}
+				})
+			) {
+				return done(null, false, { message: 'Incorrect password' });
+			}
+			return done(null, user);
+		});
+	})
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	Member.findById(id, (err, user) => {
+		done(err, user);
+	});
+});
+
+const sessionConfig = {
+	secret: process.env.PASSPORT_SECRET,
+	resave: false,
+	saveUninitialized: true,
+	store: MongoStore.create({ mongoUrl: mongoDb, collectionName: 'sessions' }),
+	cookie: { maxAge: 1000 * 60 * 60 * 24 }, // One day
+};
 
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
