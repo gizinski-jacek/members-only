71c80285a5863263a93d239ae1b33c08a7735748
mrjackmaster <jacektrg+github@gmail.com>
2022-01-12 17:51:12 +0100

    Routes for new message, sign up, member details page, log in and log out

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const message_controller = require('../controllers/messageController');
+const member_controller = require('../controllers/memberController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Fight Club' });
 });
+
+router.get('/new-message', message_controller.message_create_get);
+
+router.post('/new-message', message_controller.message_create_post);
+
+router.get('/sign-up', member_controller.member_create_get);
+
+router.post('/sign-up', member_controller.member_create_post);
+
+router.get('/log-in', member_controller.member_log_in_get);
+
+router.post('/log-in', member_controller.member_log_in_post);
+
+router.get('/log-out', member_controller.member_log_out_get);
+
+router.get('/member/:username', member_controller.member_details_get);
+
+router.post('/member/:username', member_controller.member_details_post);
 
 module.exports = router;
