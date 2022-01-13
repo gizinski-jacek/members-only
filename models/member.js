6ad99d1fb114950bbcc655743326b6ea26cfcd53
mrjackmaster <jacektrg+github@gmail.com>
2022-01-13 17:29:23 +0100

    Refactoring schema, adding virtual for membership status

--- models/member.js
+++ models/member.js
@@ -7,18 +7,32 @@ const MemberSchema = new Schema({
 	last_name: { type: String, minlength: 2, maxlength: 16, required: true },
 	username: { type: String, minlength: 4, maxlength: 16, required: true },
 	password: { type: String, required: true },
-	membership: { type: String, default: 'recruit' },
+	recruit: { type: Boolean, default: true },
+	fighter: { type: Boolean, default: false },
 	leader: { type: Boolean, default: false },
 });
 
 // Virtual for user's URL
 MemberSchema.virtual('url').get(function () {
-	return '/member/' + this._id;
+	return '/member/' + this.username;
 });
 
 // Virtual for user's full name
 MemberSchema.virtual('fullname').get(function () {
 	return `${this.first_name} ${this.last_name}`;
+});
+
+// Virtual for user's current highest membership status
+MemberSchema.virtual('memberstatus').get(function () {
+	if (this.leader) {
+		return 'Leader';
+	}
+	if (this.fighter) {
+		return 'Fighter';
+	}
+	if (this.recruit) {
+		return 'Recruit';
+	}
 });
 
 module.exports = mongoose.model('Member', MemberSchema);
