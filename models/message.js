4943bfb7e61fa137b8613ad49e00a9846b7350ea
mrjackmaster <jacektrg+github@gmail.com>
2022-01-13 17:29:41 +0100

    Minor change, adding virtual for formatted timestamp

--- models/message.js
+++ models/message.js
@@ -4,14 +4,20 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
 	title: { type: String, minlength: 4, maxlength: 16, required: true },
-	messagetext: { type: String, minlength: 8, maxlength: 128, required: true },
+	text: { type: String, minlength: 8, maxlength: 128, required: true },
 	timestamp: { type: Date, required: true },
 	author: { type: String, required: true },
+	authorid: { type: String, required: true },
 });
 
-// Virtual for user's URL
+// Virtual for message's URL
 MessageSchema.virtual('url').get(function () {
 	return this._id;
+});
+
+// Virtual for message's formatted date
+MessageSchema.virtual('timestamp_formatted').get(function () {
+	return this.timestamp.toLocaleString('en-GB');
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
